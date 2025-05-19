cf91fdd1d1695400c397fee5fd229f820b93d095
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2025-05-19 23:04:57 +0300

    fix issue

--- src/Components/ShowItemCard.jsx
+++ src/Components/ShowItemCard.jsx
@@ -44,117 +44,121 @@ function ShowItemCard({ item }) {
       {isOpenEditModel && (
         <EditingShow item={item} setIsOpenEditModel={setIsOpenEditModel} />
       )}
-      <div className="cardHeader">
-        {/* Name */}
+      {/* flex dir col */}
+      <div className="cardSplit">
         <div className="title">{item.name}</div>
-        {/* Year */}
-        {item.year > 0 ? (
-          <div className="title">{item.year}</div>
-        ) : (
-          <div>-</div>
-        )}
-        {/* Completed icon */}
-        <div className="completedIcon">
-          <span
-            style={{
-              cursor: "pointer",
-              color: item.isCompleted ? "green" : "red",
-              marginRight: "10px",
-            }}
-            onClick={() => updateIsComplete(item)}
-          >
-            {item.isCompleted ? "✔" : "✖"}
-          </span>
-        </div>
-      </div>
-      <section className="card">
-        {/* Rating */}
-        {/* 
+        <div className="card">
+          {/* flex dir row */}
+          <div className="cardHeader">
+            {/* header */}
+            {/* flex d col */}
+            {item.year > 0 ? (
+              <div className="title">{item.year}</div>
+            ) : (
+              <div>-</div>
+            )}
+            <div className="completedIcon">
+              <span
+                style={{
+                  cursor: "pointer",
+                  color: item.isCompleted ? "green" : "red",
+                  marginRight: "10px",
+                }}
+                onClick={() => updateIsComplete(item)}
+              >
+                {item.isCompleted ? "✔" : "✖"}
+              </span>
+            </div>
+          </div>
+          {/* Rating */}
+          {/* 
         <div>
           <StarRating rating={item.rating} />
         </div> */}
-        {/* Inc/Dec Season */}
-        <div className="SeasonContainer">
-          <label>Season: {item.season}</label>
-          <span className="seasonButtons">
-            <input
-              type="button"
-              className="bMinus"
-              value="-"
-              onClick={() => {
-                item.season - 1 < 1
-                  ? setError("Season Must be greater than 0")
-                  : decreaseSeason(item);
-              }}
-            />
+          {/* Inc/Dec Season */}
+          <div className="SeasonContainer">
+            <label>Season: {item.season}</label>
+            <span className="seasonButtons">
+              <input
+                type="button"
+                className="bMinus"
+                value="-"
+                onClick={() => {
+                  item.season - 1 < 1
+                    ? setError("Season Must be greater than 0")
+                    : decreaseSeason(item);
+                }}
+              />
 
-            <input
-              type="button"
-              value="+"
-              className="bPlus"
+              <input
+                type="button"
+                value="+"
+                className="bPlus"
+                onClick={() => {
+                  increaseSeason(item);
+                }}
+              />
+            </span>
+          </div>
+          {/* Inc/Dec Episode */}
+          <div className="EpisodeContainer">
+            <label>Episode: {item.episode}</label>
+            <span className="episodeButtons">
+              <input
+                type="button"
+                className="bMinus"
+                value="-"
+                onClick={() => {
+                  item.episode - 1 < 1
+                    ? setError("Episode Must be Positive")
+                    : decreaseEpisode(item);
+                }}
+              />
+              <input
+                type="button"
+                value="+"
+                className="bPlus"
+                onClick={() => {
+                  increaseEpisode(item);
+                }}
+              />
+            </span>
+          </div>
+          {/* buttons */}
+          <div className="buttons-container">
+            {/* Re-Watch */}
+            <button
+              className="tooltip-btn"
               onClick={() => {
-                increaseSeason(item);
+                reWatchShow(item);
               }}
-            />
-          </span>
+            >
+              ↩️
+              <span className="tooltip-text">Re-Watch</span>
+            </button>
+            {/* Delete */}
+            <button
+              className="tooltip-btn"
+              onClick={() => {
+                deleteAShow(item);
+              }}
+            >
+              🗑️
+              <span className="tooltip-text">Delete</span>
+            </button>
+            {/* Edit */}
+            <button
+              className="tooltip-btn"
+              title="Edit"
+              onClick={() => {
+                setIsOpenEditModel(true);
+              }}
+            >
+              ✏️
+              <span className="tooltip-text">Edit</span>
+            </button>
+          </div>
         </div>
-        {/* Inc/Dec Episode */}
-        <div className="EpisodeContainer">
-          <label>Episode: {item.episode}</label>
-          <span className="episodeButtons">
-            <input
-              type="button"
-              className="bMinus"
-              value="-"
-              onClick={() => {
-                item.episode - 1 < 1
-                  ? setError("Episode Must be Positive")
-                  : decreaseEpisode(item);
-              }}
-            />
-            <input
-              type="button"
-              value="+"
-              className="bPlus"
-              onClick={() => {
-                increaseEpisode(item);
-              }}
-            />
-          </span>
-        </div>
-      </section>
-      <div className="buttons-container">
-        {/* Re-Watch */}
-        <button
-          className="tooltip-btn"
-          onClick={() => {
-            reWatchShow(item);
-          }}
-        >
-          ↩️
-          <span className="tooltip-text">Re-Watch</span>
-        </button>
-        {/* Delete */}
-        <button
-          className="tooltip-btn"
-          onClick={() => {
-            deleteAShow(item);
-          }}
-        >
-          🗑️
-          <span className="tooltip-text">Delete</span>
-        </button>
-        {/* Edit */}
-        <button
-          className="tooltip-btn"
-          title="Edit"
-          onClick={() => {
-            setIsOpenEditModel(true);
-          }}
-        >
-          ✏️
-          <span className="tooltip-text">Edit</span>
-        </button>
       </div>
       {error && <h4>{error}</h4>}
     </section>
